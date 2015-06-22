09e5d111dad188831e2505b459259dd4508f0922
shinhua <pon0306@gmail.com>
2015-06-22 15:48:31 +0800

    [Function] Add speech function.

--- src/com/example/googletranslate/MainActivity.java
+++ src/com/example/googletranslate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.googletranslate;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -13,11 +14,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +32,12 @@ public class MainActivity extends Activity {
 	static String result = "";
 	final String tag = "Prueba";
 	TextView outtxt;
+	
+	
+	/* Speech parameter */
+    private static final String TAG = "VoiceRecognition";
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+	
 	
 	
 	/** Called when the activity is first created. */
@@ -44,8 +53,10 @@ public class MainActivity extends Activity {
 		btnSearch.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
 				
-				String txtquery = txtSearch.getText().toString();
-				callGoogleTranslate("en", "zh-TW", txtquery);
+				startVoiceRecognitionActivity();
+				
+/*				String txtquery = txtSearch.getText().toString();
+				callGoogleTranslate("en", "zh-TW", txtquery);*/
 			}
 		});
 
@@ -139,5 +150,63 @@ public class MainActivity extends Activity {
 
 		return "";
 	}
+	
+	
+	 
+    /**
+     * Fire an intent to start the speech recognition activity.
+     */
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        // Specify the calling package to identify your application
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+
+        // Display an hint to the user about what he should say.
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say Something!");
+
+        // Given an hint to the recognizer about what the user is going to say
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        // Specify how many results you want to receive. The results will be sorted
+        // where the first result is the one with higher confidence.
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+
+        Log.i(TAG,"Calling the Voice Intenet");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+	
+	
+    /**
+     * Handle the results from the recognition activity.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+        	
+     	
+          // Fill the list view with the strings the recognizer thought it could have heard, there should be 5, based on the call
+          ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+          //display results.
+          logthis("results: "+String.valueOf(matches.size())); 
+          String txtquery = matches.get(0);
+		  callGoogleTranslate("en", "zh-TW", txtquery);
+          
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    
+	/*
+	 * simple method to add the log TextView.
+	 */
+	public void logthis (String newinfo) {
+		if (newinfo != "") {
+			outtxt.setText(outtxt.getText() + "\n" + newinfo);
+		}
+	}
+    
 
 }
